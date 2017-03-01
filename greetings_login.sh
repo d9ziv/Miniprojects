e880b4c56caf1483ce4288f7a748e35210d3280b
Diego Vizcarra <dgo.vizcarra@gmail.com>
2017-03-01 01:22:26 -0600

    Create a function and a variable to hold the current time

--- greetings_login.sh
+++ greetings_login.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ $(date +%H) -lt 12 ]; then
-    cowsay "Good morning!"
-elif [ $(date +%H) -lt 20 ]; then
-    cowsay "Good afternoon!"
-else
-    cowsay "Good night!"
-fi
+_greet_time_of_day() {
+	local current_time=$(date +%H)
+	if [ $current_time -lt 12 ]; then
+    	cowsay "Good morning!"
+	elif [ $current_time -lt 20 ]; then
+	    cowsay "Good afternoon!"
+	else
+	    cowsay "Good night!"
+	fi
+}
+
+_greet_time_of_day
